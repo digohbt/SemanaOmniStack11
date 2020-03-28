a1b185e01d2571866d3a95577843018a109eebed
digohbt <digohbt@gmail.com>
2020-03-27 23:40:21 -0300

    terceiro dia

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const OngController = require('./controllers/OngController')
+const IncidentesController = require('./controllers/incidentController') 
+const ProfileController = require("./controllers/ProfileController")
+const SessionController = require("./controllers/SessionController")
  
 const routes = express.Router();
 
+routes.post('/sessions',SessionController.create )
+
 routes.get('/ongs',OngController.index );
-
-
 routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index)
+
+routes.get('/incidents', IncidentesController.index);
+routes.post('/incidents', IncidentesController.create);
+routes.delete('/incidents/:id', IncidentesController.delete);
+
+
 
 module.exports = routes
